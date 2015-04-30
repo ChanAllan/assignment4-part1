2e6b8e8aab45eab8b5a7095e5fd6c85094ac71e3
Allan Chan <chanal@onid.oregonstate.edu>
2015-04-30 01:46:07 -0700

    Created loopback.php and included a basic html skeleton

---
+++ src/loopback.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <title>Assignment4-Part1: loopback</title>
+</head>
+<body>
+
+<?php
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
