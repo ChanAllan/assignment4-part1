00eb136c2f2f3d0a90d7673b7b901b17c3380619
Allan Chan <chanal@onid.oregonstate.edu>
2015-04-30 20:56:03 -0700

    Rearranged the php tags and removed the header content type to see form.

--- src/login.php
+++ src/login.php
@@ -1,23 +1,24 @@
-<!DOCTYPE html>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo '<!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
   <link href="style.css" rel="stylesheet" type="text/css">
   <title>Assignment4-Part1: login</title>
 </head>
-<body>
-
-<?php
-error_reporting(E_ALL);
-init_set('display_errors', 1);
-header('Content-Type: text/plain');
+<body>';
 
 echo '<form action="content1.php" method="post">
     <label>Username: </label>
     <input type="text" id="username" name="username">
     <input type="submit" name="submit" value="Login">
 
-  </form>'
-?>
+  </form>';
 
-</body>
-</html>
+
+echo '</body>
+</html>';
+
+?>
