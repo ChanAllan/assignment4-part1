842432d8e0e8ab6596101a378d4342f302040e9e
Allan Chan <chanal@onid.oregonstate.edu>
2015-05-02 23:04:29 -0700

    Corrected errors with content1.php and updated with new code for logging out.

--- src/content1.php
+++ src/content1.php
@@ -1,18 +1,31 @@
-<!DOCTYPE html>
-<head>
-  <meta charset="UTF-8">
-  <link href="style.css" rel="stylesheet" type="text/css">
-  <title>Assignment4-Part1: content1</title>
-</head>
-<body>
-
 <?php
 /*Code obtained from PHP Sessions lecture video with
 modifications for the assignment requirements*/
 /*Starts the session*/
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 
 if(session_status() == PHP_SESSION_ACTIVE) {
+  ///////////////////////////////////////////////////////////////////////////////
+  /*Testing out get action*/
+  if(isset($_GET['action']) && $_GET['action'] == 'logout') {
+  /*Code obtained from Sessions lecture video with slight modifications*/
+  //session_start();
+  /*Sets session to an empty array*/
+  $_SESSION = array();
+  /*Destroys the session id*/
+  session_destroy();
+
+  /*Redirects to login.php after ending session*/
+  $filePath = explode('/', $_SERVER['PHP_SELF'], -1);
+  $filePath = implode('/',$filePath);
+  $redirect = "http://" . $_SERVER['HTTP_HOST'] . $filePath;  
+  header("Location: login.php", true);
+  die();
+  }
+  /////////////////////////////////////////////////////////////////////////////////
+
   /*Checks if username exists*/
   if(isset($_POST['username'])) {
     
@@ -49,11 +62,8 @@ if(session_status() == PHP_SESSION_ACTIVE) {
     $_SESSION['visits']++;
 
     echo 'Click <a href="content2.php">here</a> to access content2.';
-    echo 'Click <a href="logout.php">here</a> to log out.';
+    echo 'Click <a href="content1.php?action=logout">here</a> to log out.';
   }
 }
 
 ?>
-
-</body>
-</html>
